fee2b9851216bd1be17efe2f2705856c11271e21
Ajay Kumar <2492355@cognizant.com>
2026-06-03 10:06:57 +0530

    modified java code and added Add function

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
-public Hello{
+public class Hello{
     public static void main(String[] args) {
         System.out.println("Hello!!");
+        int res = add(4,5);
+        System.out.println(res);
+    }
+    public static int add(int a, int b){
+        return a+b;
     }
 }
